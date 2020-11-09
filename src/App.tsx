f8e9d8cdfe96265f0b0dc3e5d8ea86592f3a14d0
Robin Ridderholt <robin.ridderholt@headlight.se>
2020-11-09 21:02:18 +0100

    First

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Counter from "./counter/counter";
+import { CounterClass } from "./counter/counterclass";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Counter />
+      <CounterClass />
     </div>
   );
 }
